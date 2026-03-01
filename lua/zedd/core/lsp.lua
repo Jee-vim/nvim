1746446d6a10dae015c68e242edd04c340c31db5
Jee <jee.vim@proton.me>
2026-03-01 14:30:08 +0700

    refactor: include astro for autoformat

--- lua/zedd/core/lsp.lua
+++ lua/zedd/core/lsp.lua
@@ -33,7 +33,7 @@ vim.diagnostic.config({
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.nix", "*.zig", "*.ts", "*.tsx", "*.css", "*.lua" },
+  pattern = { "*.nix", "*.zig", "*.ts", "*.tsx", "*.astro", "*.css", "*.lua" },
   callback = function()
     vim.lsp.buf.format({ async = true })
   end,
