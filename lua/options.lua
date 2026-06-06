da44f6e99beb9ca4cfbaf963ccf45dc55e25ee7e
Jee <jee.vim@proton.me>
2026-06-06 23:12:27 +0700

    refactor: update options for better experience

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.backspace = "start,eol,indent"
 
 vim.o.wrap = false
 vim.o.smartindent = true
@@ -17,10 +18,22 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.laststatus = 3
 
+vim.opt.mouse = "a"
 vim.o.swapfile = false
 vim.o.backup = false
 vim.o.undofile = true
 vim.o.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.o.list = true
+vim.o.foldtext = "v:lua.fold_text()"
+vim.o.foldlevelstart = 99
+
+vim.opt.fillchars:append({
+	eob = " ",
+})
+vim.opt.listchars = {
+	tab = "  ",
+	space = " ",
+}
 
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.isfname:append("@-@")
@@ -30,3 +43,14 @@ vim.o.scrolloff = 8
 vim.o.signcolumn = "yes"
 vim.o.cmdheight = 0
 vim.o.termguicolors = true
+vim.o.cursorline = true 
+vim.o.winblend = 5
+vim.o.wildoptions = "pum"
+vim.o.pumblend = 5
+vim.o.background = "dark"
+
+function fold_text()
+	local line = vim.fn.getline(vim.v.foldstart)
+	local line_text = string.gsub(line, '^"{\\+', "")
+	return line_text
+end
