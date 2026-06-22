eda8a8e0d2f0bb2e14d213a6711170de930b0f84
Jee <jee.vim@proton.me>
2026-06-22 08:16:23 +0700

    feat: mapping for replace word globaly

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,7 @@ local map = vim.keymap.set
 map("n", "<Esc>", ":nohl<CR>", { desc = "Clear search highlighting", silent = true })
 map("n", "<leader>w", ":w<CR>", { desc = "Write", silent = true })
 map("n", "<leader>q", ":q<CR>", { desc = "Quit", silent = true })
+map("n", "<leader>re", ":restart<CR>", { desc = "Restart" })
 
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
@@ -19,16 +20,20 @@ map("v", "<C-u>", "<C-u>zz", { desc = "Move up in buffer with cursor centered" }
 map("v", "n", "nzzzv", { desc = "Next search result cursor centered" })
 map("v", "N", "Nzzzv", { desc = "Prev search result cursor centered" })
 
-map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word cursor" })
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], {
+  desc = "Replace word under cursor"
+})
+map("n", "<leader>S",
+  [[:let @z=expand('<cword>')<CR>:vimgrep /<C-r>z/ `git ls-files`<CR>:copen<CR>:cfdo %s/<C-r>z//ge | update<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>]],
+  { desc = "Replace word cursor all files" }
+)
 map("n", "<leader>n", function ()
   local word = vim.fn.expand("<cword>")
   if word ~= "" then
     vim.fn.setreg("/", "\\<" .. word .. "\\>")
     vim.cmd("normal! n")
   end
-end, { desc = "Replace word cursor" })
-map("n", "<leader>x", "<CMD>!chmod +x %<CR>", { desc = "Make file executable", silent = true })
-map("n", "<leader>re", ":restart<CR>", { desc = "Restart" })
+end, { desc = "Search word cursor" })
 
 map("n", "<leader>bd", "<CMD>bd<CR>", { desc = "Close buffer" })
 map("n", "<leader>ba", "<CMD>%bd|e#<CR>", { desc = "Close all buffers except current" })
